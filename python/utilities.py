49dec7f584b2ab58776d2c7a0f3af351e1e8316c
Devin Taylor <dntaylor@wisc.edu>
2017-07-11 11:37:49 -0500

    collisions17 first golden json

--- python/utilities.py
+++ python/utilities.py
@@ -23,6 +23,9 @@ jsons = {
     'Collisions16': '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/'\
                     'Collisions16/13TeV/'\
                     'ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt', # 36.46/fb
+    'Collisions17': '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/'\
+                    'Collisions16/13TeV/'\
+                    'PromptReco/Cert_294927-297723_13TeV_PromptReco_Collisions17_JSON.txt', # 3.785/fb
 }
 
 def getJson(runPeriod):
